88b75a009f437a58b26905960501352bf0c4575f
Mikael <nackoza@outlook.com>
2020-09-16 19:31:27 +0200

    Jag ändrade lite i method classen, lade in lite methoder osv, känns bara som det är de olika metoderna för att updatera hersianen kvar

--- Project_2.py
+++ Project_2.py
@@ -8,98 +8,160 @@ Created on Sat Sep 12 17:20:59 2020
 from scipy import *
 from matplotlib.pyplot import *
 import timeit
-
-
-class Optimazation_problem:
-    
-    
-    
-    def __init__( self , Gradient = 0 ):
-        
-        if not Gradient:
-            from import_function import * 
-            
-            self.Gradient_check = True
-            
-        else:
-            from import_function import * 
-            
-            self.Gradient_check = False
-    
-    
     
 class Optimazation_methods:
     
-    import Optimazation_problem
-    
-    
-    
-    
-    
-    def Newton( self , x_k , simplified = False , k_max = 100):
+    def Grad_F( self , X ):
         
-        ### INITIAL CONSTANTS/GUESS
-        A = self.Jacobian( x_k )
-        k = 0
-        tol = 1e-5
-        b = - self.F( x_k )
-        x_kp1 = x_k.copy()
-        ### INITIAL CONSTANTS
-        
-        while norm( b ) <= tol and k < k_max :   # Default 100 newton iterations
-            
-            k += 1
-            x_k = x_kp1.copy()
-            
-            if simplified:
-                
-                x_delta = self.Cholesky_method( A , b )     # This should be fixed
-                
-                x_kp1 = x_delta + x_k
-                b = - self.F( x_kp1 )
-                
-            else:
-                
-                x_delta = self.Cholesky_method( A , b )    # This should be fixed
-                
-                x_kp1 = x_delta + x_k
-                A = self.Jacobian( x_kp1 )
-                b = - self.F( x_kp1 )
-        
-        return x_np1
-        
-        
-        
-    def Grad_F2(self,X):
-        
-        l = shape(X)[0]
-        
-        
-            
-
-    '''
-    def jac(self,u_n, u_0):
         h = 1e-4
-        N = u_n.shape[0]
-        JacF = zeros((N,N))
-        H = zeros(N)
-        
-        for i in range(N):
+        n = shape( X )[0]
+        grad_vector = zeros(n)
+        H = zeros(n)
+   
+        for i in range(n):
             H[i] = h
-            JacF[:,i] = (self.F(u_n + H, u_0) - self.F(u_n, u_0))/h
+            grad_vector[i] = ( self.F( X + H ) - self.F( X ) ) /h
             H[i] = 0
         
-        return JacF
-    '''
+        return grad_vector
+    
+    def Hersian( self , X ):
+        h = 1e-4
+        n = X.shape[0]
+        Hersian = zeros( (n,n) )
+        H = zeros(n)
         
-    def Grad_F(self, x_1 , x_2 ):
+        for i in range(n):
+            H[i] = h
+            Hersian[:,i] = ( self.Grad_F( X + H ) - self.Grad_F( X ) )/h
+            H[i] = 0
+        
+        Her_F = (1/2) * ( Hersian + Hersian.T )
+        
+        if all( eigvals( Her_F ) > 0 ):
+            
+            return Her_F
+        
+        
+        else:
+            raise Exception( "Sorry The Hersian Is Not Positive Definite, Try Again" )
+            
+    def Newton( self , Method_for_a , Method_for_H ):
+        
+        x_k = self.initial_x.copy()
+        grad_f_k = self.Grad_F(x_k)
+        Her_f_k = self.Hersian(x_k)
+        H_k = inv(Her_f_k)
+        k = 0
+        
+        while norm( self.Grad_F( x_k ) ) > 1e-7 and k <= 100:
+            
+            k += 1
+            s_k = - dot( H_k , grad_f_k ) 
+            x_kp1 = x_k + self.alpha( Method_for_a , x_k , s_k ) * ( s_k )
+            x_k = x_kp1.copy()
+            grad_f_k = self.Grad_F( x_k )
+            ####H_k = self.Update_Hersian( Method_for_h , x_k , H_k ,  )
+            
+        if all( eigvals( H_k ) > 0 ) and k != 100:
+            
+            return x_k
+        else:
+            raise Exception ("k = " ,k , " If k == 100 you did not converge, otherwise your hersian is not PD " )
+            
+        
+    def alpha( self , Method_for_a , x_k , s_k ):
+        
+        Classical = [['Classical']]  ##### Detta kan man säkert göra snyggare, men det var typ såhär jag tänkte
+        Inexact = [['Inexact']]      #####
+        Exact = [['Exact']]          #####
+        
+        if Method_for_a in Classical:
+            return - 1
+        elif Method_for_a in Inexact:
+            return self.inexact_a( x_k , s_k )
+        elif Method_for_a in Exact:
+            return self.exact_a( x_k , s_k )
+        else:
+            raise Exception("You have not choosen appropriet method for alpha method . Use format: Method_for_a = ['Classical'],['Inexact'] or ['Exact']")
+    
+        
+    def exact_a( self , x_k , s_k ):
+        fa = lambda a : self.F( x_k + a * s_k )
+        a = scipy.optimize.minimize(fa)
+        return a
+        
+    def inexact_a( self , x_k , s_k ):
+        
+        a_l = 0
+        a_r = 1e7
+        sigma = 0.7
+        rho = 0.1
+        a_0 = (a_l + a_r) / 2
+        
+        fa = lambda a : self.F( x_k + a * s_k )
+        fa_prim_a_0 = self.Grad_F_a( a_0 , x_k , s_k ) 
+        f_a_prim_a_l = self.Grad_F_a( a_l , x_k , s_k )
+        fa_a_0 = fa(a_0)
+        f_a_a_l = fa(a_l)
+        
+        while not ( fa_prim_a_0 >= f_a_prim_a_l  ) and not ( fa_a_0 <= fa_a_l + rho*f_a_prim_a_l ):
+            
+            if not ( fa_prim_a_0 >= f_a_prim_a_l  ):
+                
+                a_0 , a_r , a_l = self.Block1( a_0 , a_l , a_r , fa_a_0 , fa_a_l , fa_prim_a_0 , f_a_prim_a_l ) 
+            
+            else:
+                
+                a_0 , a_r , a_l = self.Block2( a_0 , a_l , a_r , fa_a_0 , fa_a_l , fa_prim_a_0 , f_a_prim_a_l )
+                
+        return a_0 
+            
+    
+    def Block1( self , a_0 , a_l , a_r , fa_a_0 , fa_a_l , fa_prim_a_0 , f_a_prim_a_l ):
+        
+        tao = 0.1
+        Chi = 9.
+        
+        delta_a_0 = ( a_0 - a_l ) * ( ( fa_prim_a_0 ) / fa_prim_a_l - fa_prim_a_0 )
+        delta_a_0 = max( delta_a_0 , tao * ( a_0 - a_l ) )
+        delta_a_0 = min( delta_a_0 , Chi* ( a_0 - a_l ) )
+        a_l = a_0
+        a_0 = a_0 + delta_a_0
+        
+        return a_0 , a_r , a_l
+        
+        
+    def Block2( self , a_0 , a_l , a_r , fa_a_0 , fa_a_l , fa_prim_a_0 , f_a_prim_a_l ):
+        
+        tao = 0.1
+        
+        a_r = min( a_0 , a_r )
+        a_0_bar =  ((( ( a_0 - a_l )**2 )*  f_a_prim_a_l ) / (2*( fa_a_l - fa_a_0 + ( a_0 - a_l )*f_a_prim_a_l )))  ## denna raden blev lite ful ?
+        a_0_bar = max ( a_0_bar , a_l + tao * ( a_r - a_l ) )
+        a_0_bar = min( a_0_bar , a_u - tao * ( a_r - a_l ) )
+        a_0 = a_0_bar
+        
+        return a_0 , a_r , a_l
+        
+        
+    
+            
+    def Grad_F_a( self , X , x_k , s_k ):
         
         h = 1e-4
+        n = shape( X )[0]
+        grad_vector = zeros(n)
+        H = zeros(n)
+        F_a = lambda a : self.F( x_k + a * s_k )
+        for i in range(n):
+            H[i] = h
+            grad_vector[i] = ( Fa( X + H ) - F_a( X ) ) /h
+            H[i] = 0
         
-        
-        return array( [ (self.F( x_1 + h , x_2 ) - self.F( x_1 , x_2 ) )/ h , (self.F( x_1 , x_2 + h) - self.F( x_1 ,x_2 ) ) / h ] )
-
-
+        return grad_vector
+    
+    
         
     
         
