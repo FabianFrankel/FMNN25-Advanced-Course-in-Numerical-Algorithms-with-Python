344e70d872f4edb7064160d344fe297d087a7348
Mikael <nackoza@outlook.com>
2020-09-18 20:28:56 +0200

    Jag ändrade lite i koden, nu funkar Good broyden och DFP med inexact alpha, men bara om man har en bra första gissning, dock så vill inte inexact alpha functionen ändra a_0, du kanske kan titta på while loopen, för jag tycker den verkar stämma. Jag kollade på exact alpha functionen så nu kan man köra den också. Jag vet inte om Bad Broyden och BFGS inte funkar för de är fel eller för att det är en dum function som vi fått.Du kanske kan kolla på dem också. Jag lade också till en sak i problem classen så man kan ge en gradian, jag ändrade därför lite i metod classen så att när man kallar på hersian functionen så går man istället till en hersian_choice function. ~

--- Method_Class.py
+++ Method_Class.py
@@ -8,16 +8,19 @@ Created on Sat Sep 12 17:20:59 2020
 from scipy import *
 from matplotlib.pyplot import *
 from numpy import *
+from scipy import optimize as so
 import timeit
 
 class Optimisation_problem:
 
-    def __init__(self, f):
+    def __init__(self, f,Grad = False):
         self.f = f
-
-    def print_func(self, x):
-        for i in x:
-            print(i, self.f(i))
+        if Grad:
+            self.Grad_f = Grad
+            self.Choice_grad = False
+        else:
+            self.Grad_f = Grad
+            self.Choice_grad = True
 
 class Optimazation_methods:
 
@@ -29,13 +32,13 @@ class Optimazation_methods:
         
         h = 1e-4
         n = shape( X )[0]
-        print(n)
+        
         grad_vector = zeros(n)
         H = zeros(n)
    
         for i in range(n):
             H[i] = h
-            grad_vector[i] = ( self.optimisation_problem.f( X + H ) - self.optimisation_problem.f( X ) ) /h
+            grad_vector[i] = ( self.optimisation_problem.f( X + H ) - self.optimisation_problem.f( X ) ) / h
             H[i] = 0
         
         return grad_vector
@@ -49,7 +52,7 @@ class Optimazation_methods:
         
         for i in range(n):
             H[i] = h
-            Hersian[:,i] = ( self.Grad_F( X + H ) - self.Grad_F( X ) )/h
+            Hersian[:,i] = ( self.Grad_F( X + H ) - self.Grad_F( X ) ) / h
             H[i] = 0
         
         Her_F = (1/2) * ( Hersian + Hersian.T )
@@ -62,83 +65,112 @@ class Optimazation_methods:
         else:
             raise Exception( "Sorry The Hersian Is Not Positive Definite, Try Again" )
             
-    def Newton( self , Method_for_a , Method_for_H ):
+  
+    def Newton( self , Method_for_a , Method_for_H_update):
         
         x_k = self.initial_x.copy()
-        helper = [1e-5]*(shape(x_k)[0])
-        Ghost_cell = array(helper)
-        x_km1 = x_k - Ghost_cell
-        grad_f_km1 = self.Grad_F(x_km1)
-        grad_f_k = self.Grad_F(x_k)
-        Her_f_k = self.Hersian(x_k)
-        H_k = linalg.inv(Her_f_k)
+        '''
+        helper = [ 1e-3 ] * ( shape( x_k )[0] )
+        Ghost_cell = array( helper )
+        x_km1 = x_k - Ghost_cell     ##### när man updaterar Hersian behövs x_k - x_km1, vet inte om man kan göra såhär
+        grad_f_km1 = self.Grad_F( x_km1 )
+        '''
+        grad_f_k = self.Grad_F( x_k )
+        Her_f_k = self.Hersian_choice( x_k )
+        H_k = linalg.inv( Her_f_k )
+        print(H_k,'H_k')
         k = 0
-        
-        while linalg.norm( self.Grad_F( x_k ) ) > 1e-7 and k < 100:
-            
+        kmax = 100000
+
+        while linalg.norm( self.Grad_F( x_k ) ) > 1e-7 and k < kmax:
+            print(linalg.norm( self.Grad_F( x_k ) ),'norm')
             k += 1
-            s_k = - dot( H_k , grad_f_k ) 
+            #print(k)
+            s_k =  dot( - H_k , grad_f_k ) 
+            #print(s_k,'sk')
             x_kp1 = x_k + self.alpha( Method_for_a , x_k , s_k ) * ( s_k )
-            
+            #print(x_kp1,'x_kp1')
             x_km1 = x_k.copy()
             x_k = x_kp1.copy()
-            H_k = self.Update_Hersian( Method_for_H ,  H_k , x_k , x_km1 , grad_f_k , grad_f_km1 )
             grad_f_k = self.Grad_F( x_k )
             grad_f_km1 = self.Grad_F( x_km1 )
+            H_k = self.Update_Hersian( Method_for_H_update ,  H_k , x_k , x_km1 , grad_f_k , grad_f_km1 )
+            #print(H_k,'Update H_k')
+
             
             
-        if all( linalg.eigvals( H_k ) > 0 ) and k != 100:
             
-            return x_k
+        if all( linalg.eigvals( H_k ) > 0 ) and k != kmax and linalg.norm( self.Grad_F( x_k ) ) != nan:
+            print('Newton converged')
+            
+            return x_k 
         else:
-            print(linalg.eigvals(H_k))
-            raise Exception ("k = " ,k , " If k == 100 you did not converge, otherwise your hersian is not PD " )
+            print('Your out of the loop but f(x_k = )',self.optimisation_problem.f( x_k ))
+            print( 'eigval(H_k' , linalg.eigvals( H_k ))
+            raise Exception ("k = " ,k , " If k == kmax you did not converge, otherwise your hersian is not PD or Grad_f(x) is nan" )
             
+      
+    def Hersian_choice( self , x_k ):
+
+        if self.optimisation_problem.Choice_grad:
+            return self.Hersian( x_k )
+        else:
+            return self.optimisation_problem.Grad_f( x_k )
+
     def alpha( self , Method_for_a , x_k , s_k ):
         
-        Classical = [['Classical']]  ##### Detta kan man säkert göra snyggare, men det var typ såhär jag tänkte
-        Inexact = [['Inexact']]      #####
-        Exact = [['Exact']]          #####
-        
-        if Method_for_a in Classical:
-            return - 1
-        elif Method_for_a in Inexact:
+        if Method_for_a == 'Classical':
+            return  1
+        elif Method_for_a == 'Inexact':
             return self.inexact_a( x_k , s_k )
-        elif Method_for_a in Exact:
+        elif Method_for_a == 'Exact':
             return self.exact_a( x_k , s_k )
         else:
-            raise Exception("You have not choosen appropriet method for alpha method . Use format: Method_for_a = ['Classical'],['Inexact'] or ['Exact']")
+            raise Exception("You have not choosen appropriet method for alpha method . Use format: Method_for_a = 'Classical','Inexact' or 'Exact' ")
     
         
     def exact_a( self , x_k , s_k ):
-        fa = lambda a : self.F( x_k + a * s_k )
-        a = scipy.optimize.minimize(fa)
-        return a
+
+        fa = lambda a : self.optimisation_problem.f( x_k + a * s_k )
+        a = so.minimize(fa , 1)
+
+        return a.x
         
     def inexact_a( self , x_k , s_k ):
         
         a_l = 0
-        a_r = 1e7
+        a_r = 1e20
         sigma = 0.7
-        rho = 0.1
-        a_0 = (a_l + a_r) / 2
+        rho = 0.7
+        a_0 = 0.7  #### jag vet ej vad a_0 skall vara, detta skall nog vara en bra gissning, vi vill ta typ a_0 steg
         
         fa = lambda a : self.optimisation_problem.f( x_k + a * s_k )
-        fa_prim_a_0 = self.Grad_F_a( a_0 , x_k , s_k ) 
-        f_a_prim_a_l = self.Grad_F_a( a_l , x_k , s_k )
+        fa_prim_a_0 = self.f_a_prim( a_0 , x_k , s_k ) 
+        f_a_prim_a_l = self.f_a_prim( a_l , x_k , s_k )
         fa_a_0 = fa(a_0)
         f_a_a_l = fa(a_l)
         
-        while not (( fa_prim_a_0 >= f_a_prim_a_l  ) or ( fa_a_0 <= fa_a_l + rho*f_a_prim_a_l )):
+        while  not ( ( fa_prim_a_0 >= sigma*f_a_prim_a_l  ) or ( fa_a_0 <= f_a_a_l + rho*(a_0 - a_l)*f_a_prim_a_l ) ) :
             
-            if not ( fa_prim_a_0 >= f_a_prim_a_l  ):
+            if  not ( fa_prim_a_0 >= sigma*f_a_prim_a_l ):
+                print('Block1' ,a_0 , a_r , a_l )
+                pause(0.1)
+                a_0 , a_r , a_l = self.Block1( a_0 , a_l , a_r , fa_a_0 , f_a_a_l , fa_prim_a_0 , f_a_prim_a_l ) 
+                fa_a_0 = fa(a_0)
+                f_a_a_l = fa(a_l)
+                fa_prim_a_0 = self.f_a_prim( a_0 , x_k , s_k ) 
+                f_a_prim_a_l = self.f_a_prim( a_l , x_k , s_k )
                 
-                a_0 , a_r , a_l = self.Block1( a_0 , a_l , a_r , fa_a_0 , fa_a_l , fa_prim_a_0 , f_a_prim_a_l ) 
-            
             else:
-                
-                a_0 , a_r , a_l = self.Block2( a_0 , a_l , a_r , fa_a_0 , fa_a_l , fa_prim_a_0 , f_a_prim_a_l )
-                
+                print('Block2',a_0 , a_r , a_l)
+                pause(0.1)
+                a_0 , a_r , a_l = self.Block2( a_0 , a_l , a_r , fa_a_0 , f_a_a_l , fa_prim_a_0 , f_a_prim_a_l )
+                fa_a_0 = fa(a_0)
+                f_a_a_l = fa(a_l)
+                fa_prim_a_0 = self.f_a_prim( a_0 , x_k , s_k ) 
+                f_a_prim_a_l = self.f_a_prim( a_l , x_k , s_k )
+
+        print(a_0,'a_0 out')
         return a_0 
     
     def Block1( self , a_0 , a_l , a_r , fa_a_0 , fa_a_l , fa_prim_a_0 , f_a_prim_a_l ):
@@ -146,9 +178,9 @@ class Optimazation_methods:
         tao = 0.1
         Chi = 9.
         
-        delta_a_0 = ( a_0 - a_l ) * ( ( fa_prim_a_0 ) / fa_prim_a_l - fa_prim_a_0 )
+        delta_a_0 = ( a_0 - a_l ) * ( ( fa_prim_a_0 ) / (f_a_prim_a_l - fa_prim_a_0) )
         delta_a_0 = max( delta_a_0 , tao * ( a_0 - a_l ) )
-        delta_a_0 = min( delta_a_0 , Chi* ( a_0 - a_l ) )
+        delta_a_0 = min( delta_a_0 , Chi * ( a_0 - a_l ) )
         a_l = a_0
         a_0 = a_0 + delta_a_0
         
@@ -159,51 +191,45 @@ class Optimazation_methods:
         tao = 0.1
         
         a_r = min( a_0 , a_r )
-        a_0_bar =  ((( ( a_0 - a_l )**2 )*  f_a_prim_a_l ) / (2*( fa_a_l - fa_a_0 + ( a_0 - a_l )*f_a_prim_a_l )))  
+        a_0_bar =  (( ( a_0 - a_l )**2 ) *  f_a_prim_a_l ) / (2*( fa_a_l - fa_a_0 + ( a_0 - a_l )*f_a_prim_a_l ))
         a_0_bar = max ( a_0_bar , a_l + tao * ( a_r - a_l ) )
-        a_0_bar = min( a_0_bar , a_u - tao * ( a_r - a_l ) )
+        a_0_bar = min( a_0_bar , a_r - tao * ( a_r - a_l ) )
         a_0 = a_0_bar
         
         return a_0 , a_r , a_l
             
-    def Grad_F_a( self , alfa , x_k , s_k ):
+    def f_a_prim( self , a , x_k , s_k ):
         
         h = 1e-4
         F_a = lambda a : self.optimisation_problem.f( x_k + a * s_k )
-        return (F_a(alfa+h)-F_a(alfa))/h
+        return ( F_a( a + h ) - F_a( a ) ) / h
     
-    def Update_Hersian( self , Method_for_h , H_k , x_k , x_km1 , grad_f_k , grad_f_km1  ):
-        
-        
-        Good_Broyden = [['Good Broyden']]  ##### Detta kan man säkert göra snyggare, men det var typ såhär jag tänkte
-        Bad_Broyden = [['Bad Broyden']]    #####
-        DFP = [['DFP']]                    #####
-        BFGS = [['BFGS']]                  #####
+    def Update_Hersian( self , Method_for_H_update , H_k , x_k , x_km1 , grad_f_k , grad_f_km1  ):
+                         
         delta_k = x_k - x_km1
         gamma_k = grad_f_k - grad_f_km1 
         
-        if Method_for_h in Good_Broyden:
+        if Method_for_H_update == 'Good Broyden':
             return self.Good_Broyden( H_k , delta_k , gamma_k )
-        elif Method_for_h in Bad_Broyden:
+        elif Method_for_H_update == 'Bad Broyden':
             return self.Bad_Broyden( H_k , delta_k , gamma_k )
-        elif Method_for_h in DFP:
+        elif Method_for_H_update == 'DFP':
             return self.DFP( H_k , delta_k , gamma_k )
-        elif Method_for_h in BFGS:
+        elif Method_for_H_update == 'BFGS':
             return self.BFGS( H_k , delta_k , gamma_k )
         else:
-            raise Exception("You have not choosen appropriet method for alpha method . Use format: Method_for_H = ['Good Broyden'],['bad Broyden'], ['DFP'] or ['BFGS'] ")
+            raise Exception("You have not choosen appropriet method for alpha method . Use format: Method_for_H = 'Good Broyden','bad Broyden', 'DFP' or 'BFGS' ")
     
-    def Good_Broyden( self , H_k , delta_k , gamma_k ):     ##### Jag tror detta är Bad Broyden, men jag kan blandat ihop dem
+    def Bad_Broyden( self , H_k , delta_k , gamma_k ):     
         
         H_k_gamma_k = dot( H_k , gamma_k )
         H_k_delta_k = dot( H_k , delta_k )
         denumerator = inner(H_k_delta_k ,gamma_k)
-        
-        H_kp1 = H_k + outer( gamma_k - H_k_gamma_k , H_k_delta_k )/denumerator
+        H_kp1 = H_k + outer( gamma_k - H_k_gamma_k , H_k_delta_k ) / denumerator
         
         return H_kp1
     
-    def Bad_Broyden( self , H_k , delta_k , gamma_k ): 
+    def Good_Broyden( self , H_k , delta_k , gamma_k ): 
         
         H_kp1 = H_k + outer( ( delta_k - dot( H_k , gamma_k) )/inner( gamma_k , gamma_k )   , gamma_k )
         
@@ -257,16 +283,17 @@ class Optimazation_methods:
 def f(X):
     return 100*(X[1]-X[0]**2)**2+(1-X[0])**2
 
-x_intital = array([1.1,1.1])
-print(shape(x_intital))
+
+
+
+x_intital = array([1.00004,1.00002])
+
 op = Optimisation_problem(f)
 
 om = Optimazation_methods(op, x_intital)
 
-print(om.Newton(['Inexact'], ['Bad Broyden']))
+print(om.Newton('Exact', 'Good Broyden'))
 
-
-print(f(array([-2.1812850e+29, 3.6140982e+30])))
 
 
 start_time = time.time()
